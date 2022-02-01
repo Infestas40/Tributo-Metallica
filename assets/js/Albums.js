86e07362624eda20aa1cc621e938d1142207167c
unknown <Infestas36>
2022-02-01 14:12:02 +0100

    subidacasa

---
+++ assets/js/Albums.js
@@ -0,0 +1,26 @@
+
+              function msgA(){
+                  alert ("HARDWIRED TO SELF DESTRUCT $24,99")
+              }
+
+              function msgB(){
+                  alert ("DEATH MAGNETIC $15,99")
+              }
+
+              function msgC(){
+                  alert ("LOAD $18,99")
+              }
+
+              function msgD(){
+                  alert ("METALLICA $21,99")
+              }
+
+              function msgE(){
+                  alert ("AND JUSTICE FOR ALL $23,99")
+              }
+
+              function msgF(){
+                  alert ("MASTER OF PUPPETS $15,99")
+              }
+
+      
